719a1eb6cfc3646fd68c02a4a4ad3fa306c0d3ab
yanrivera19 <yan.rivera.6@gmail.com>
2021-11-25 05:34:55 -0800

    Project 9 completed. Successfully loads ToDos date from API on page load when the localstorage is empty.

--- script.js
+++ script.js
@@ -2,86 +2,128 @@
 ToDo App
 
 Step 1: Select the input box by its id name to retrieve any value that gets submitted in it, and
-select the <ul> list from the HTML file by its id name. The tasks will be added to and displayed on
+select the <ul> list from the HTML file by its id name. The todos will be added to and displayed on
 this <ul>.
-Step 2: Call the displayOnPage function, that will display the values stored in the localstorage inside of the 
+Step 2: Create a function expression called createList that takes in "todos" as its parameter. 
+This function will create a set of elements for each "todo" in the API (JSON Placeholder) that will be fetched in the next function. The parameter 
+"todos" refers to the response data that you receive when fetching the API if there are no errors in the request. When this function gets executed, it will 
+display on the page load these elements with the response data in them.
+Step 3: Create a function called getTodos that will fetch the API using axios if the length of the array in the
+localstorage is 0. What this means is that, on page load, if the localstorage is empty, it will fetch the API
+to get the "todos" resources from it, and if there are no errors, the response of the "get" method used will call
+the createList function previously defined with the response date in its parameter. Call this function (getTodos).
+Step 4: Call the displayOnPage function, that will display the values stored in the localstorage inside of the 
 <ul>.
-Step 2: Create a function called addToList that creates a set of elements on which the tasks submitted on the input 
+Step 5: Create a function called addToList that creates a set of elements on which the todos submitted on the input 
 box will be displayed. This function gets executed whenever an input is submitted, and takes in an event as its parameter.
 It will first check if the input submitted has a valid value. If it doesn't have one, it will display an alert message asking the user
-to enter a task. If it does have one, it will create an a <li> and a checkbox button for each of the input values submitted,
-it will call a function called saveTasks that will save these input values to the localstorage, and whenever an input gets submitted,
+to enter a todo. If it does have one, it will create an a <li> and a checkbox button for each of the input values submitted,
+it will call a function called savetodos that will save these input values to the localstorage, and whenever an input gets submitted,
 the input box will be cleared.
-Step 3: Create a function called saveTasks that takes in a task as a parameter. This task refers to the input value that was submitted
-and evaluated on the addToList function. The saveTasks function is called inside the addToList function and takes in as its parapeter the
-input value being evaluated. The saveTasks function first checks if the local storage is empty. If it is, it returns an empty array called "list". 
+Step 6: Create a function called saveTodosLocalstorage that takes in a todo as a parameter. This todo refers to the input value that was submitted
+and evaluated on the addToList function. The savetodos function is called inside the addToList function and takes in as its parapeter the
+input value being evaluated. The savetodos function first checks if the local storage is empty. If it is, it returns an empty array called "list". 
 If it is not, it returns the value/s of the specified key from the storage by using the JSON.parse method, and the value gets added into the "list" 
-array. In this case, the name of the localstorage key is also "list".The function will also make every task or input value that gets passed in be 
+array. In this case, the name of the localstorage key is also "list".The function will also make every todo or input value that gets passed in be 
 inserted inside of the "list" array by using the push method, and later they will be stored to the localstorage as a string, by using the JSON.stringify
 method.
-Step 5: Create a function called displayOnPage, that firstly defines a variable called "list" with the retrieval of the values in the localstorage as an array.
+Step 7: Create a function called displayOnPage, that firstly defines a variable called "list" with the retrieval of the values in the localstorage as an array.
 To this array, use the forEach method to run an annonymous function for each of the individual item in the "list" array. This function will take 
-in a task as its parameter. This "task" refers to each individual item of the array. For each individual item, the same things that were created on the addToList
+in a todo as its parameter. This "todo" refers to each individual item of the array. For each individual item, the same things that were created on the addToList
 function will be created, and the values from the localstorage will be appended to them.
-Step 6: Create a function called deleteChecked that removes from the UI and the local storage the selected list items on the UI. The list items are selected by
+Step 8: Create a function called deleteChecked that removes from the UI and the local storage the selected list items on the UI. The list items are selected by
 the checking of the checkbox next to them, and then are removed from the page and localstorage by clicking the trash bin button that is underneath the input box on the
 UI. In order to do this, the checkboxes have to be looped through with a for looped. If the current checkbox being looped is checked, the localstorage values are retrieved 
 as an array and, with the use of the splice method, remove the current checkbox ([i]) from the array.Then, update the localstorage by storing the modified array
 into it, and finally remove the checkbox from the UI using the the remove method.
 */
 
-let inputValue = document.getElementById("inputTask").value;
-const taskList = document.getElementById("taskList");
+let inputValue = document.getElementById("inputtodo").value;
+let todoList = document.getElementById("todoList");
 
+/* 
+
+*/
+
+const createList = (todos) => {
+    todos.forEach(todo => {        
+	    const listItm = document.createElement("LI");
+		listItm.className = "list-group-item";		
+		const inputCheckBox = document.createElement("INPUT");
+		inputCheckBox.className = "delete-box form-check-input mr-3";
+		inputCheckBox.type = "checkbox"
+		const inputValueContent = document.createTextNode(todo.title);
+		listItm.appendChild(inputCheckBox);
+		listItm.appendChild(inputValueContent);
+	    todoList.appendChild(listItm);
+    });
+};
+
+const getTodos = () => {
+	let arrayTodos = JSON.parse(localStorage.getItem("list"));
+
+	if(arrayTodos.length === 0) {
+	    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+	        .then(response => {
+	        	console.log(response);
+	            const todos = response.data;
+	            console.log(`GET list todos`, todos);
+	            createList(todos);
+	        })
+	        .catch(error => console.error(error));
+    }
+};
+
+getTodos();
 displayOnPage();
 
 function addToList(event) {
 	event.preventDefault();
 	
-	inputValue = document.getElementById("inputTask").value;	
+	inputValue = document.getElementById("inputtodo").value;	
 	
 	if (inputValue === "" || inputValue === "null") {
-    	alert("Enter a task");
+    	alert("Enter a todo");
     	return false;
   	} else {
 		const listItm = document.createElement("LI");
 		listItm.className = "list-group-item";
-		taskList.appendChild(listItm);
+		todoList.appendChild(listItm);
 		const inputCheckBox = document.createElement("INPUT");
 		inputCheckBox.className = "delete-box form-check-input";
 		inputCheckBox.type = "checkbox"
-		inputValue = document.getElementById("inputTask").value;	
+		inputValue = document.getElementById("inputtodo").value;	
 		const inputValueContent = document.createTextNode(inputValue);
 	  	listItm.appendChild(inputCheckBox);
 	  	listItm.appendChild(inputValueContent);
-	  	saveTasks(inputValue);
-	  	document.getElementById("inputTask").value = ""; 	  	
+	  	saveTodosLocalstorage(inputValue);
+	  	document.getElementById("inputtodo").value = ""; 	  	
   	} 
 };
 
-function saveTasks(task) {
+function saveTodosLocalstorage(todo) {
 	let list;
 	if(localStorage.getItem("list") === null) {
 		list = [];
 	} else {
 		list = JSON.parse(localStorage.getItem("list"));
 	};	
-	
-	list.push(task);
+
+	list.push(todo);
 	localStorage.setItem("list", JSON.stringify(list));
 };
 
 function displayOnPage() {
 	const list = JSON.parse(localStorage.getItem("list"));
 
-	list.forEach(function(task) {
+	list.forEach(function(todo) {
 	const listItm = document.createElement("LI");
 	listItm.className = "list-group-item";
-	taskList.appendChild(listItm);
+	todoList.appendChild(listItm);
 	const inputCheckBox = document.createElement("INPUT");
 	inputCheckBox.className = "delete-box form-check-input mr-3";
 	inputCheckBox.type = "checkbox"
-	const inputValueContent = document.createTextNode(task);
+	const inputValueContent = document.createTextNode(todo);
   	listItm.appendChild(inputCheckBox);
   	listItm.appendChild(inputValueContent);
 	});
